8126528f3523e0cda1cb9822f413d56e017f14ae
jvorndran <135787855+jvorndran@users.noreply.github.com>
2023-10-30 19:26:28 -0400

    too many changes to bother

--- src/SymbolEntry.java
+++ src/SymbolEntry.java
@@ -1,16 +1,12 @@
 public class SymbolEntry<T> {
-
     String name;
     String type;
     T value = null;
-
     SymbolEntry<?> next;
-
-    public SymbolEntry(String name, String type, T value){
+    public SymbolEntry(String name, String type, T value) {
         this.name = name;
         this.type = type;
         this.value = value;
         this.next = null;
     }
-
 }
